226c2fea128640a5b9a8cc1b13d6f4a8070ad479
unknown <afergusonlewis@gmail.com>
2023-12-01 15:30:17 +0000

    GG

---
+++ day01/solution2.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from typing import Iterable
+
+from utils.read import read_file_iter
+
+
+# Use the following line instead if the input is a single line
+# def solver(input: str) -> str:
+def solver(input: Iterable[str]) -> str:
+    return "the other answer"
+
+
+if __name__ == "__main__":
+    input_path = Path(__file__).parent / "input.txt"
+    input_iter = read_file_iter(input_path)
+
+    # Use the following line instead if the input is a single line
+    # print(solver(next(input_iter)), end="")
+    print(solver(input_iter), end="")
---
+++ day01/test_solution1.py
@@ -0,0 +1,5 @@
+from . import solution1
+
+
+def test_solution1():
+    assert solution1.solver([]) == "the answer"
